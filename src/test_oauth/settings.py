f57526a7608aa1345fcf5786441b3597a393a930
Anton Kulinsky <ankulinsky@gmail.com>
2018-07-26 14:14:34 +0500

    settings

--- src/test_oauth/settings.py
+++ src/test_oauth/settings.py
@@ -130,9 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATICFILES_DIRS = []
 
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
